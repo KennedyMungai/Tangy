01c218cdfbfc12cb29d40f30271a9a81a4add5d1
KennedyMungai <kennedymungaifmab@gmail.com>
2022-05-02 21:36:31 +0300

    I'm done with javascript inside a blazor app

---
+++ TangyWeb_Server/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace TangyWeb_Server.Helper
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async ValueTask ToastrSuccess(this IJSRuntimeExtension JsRuntime, string message)
+        {
+            await JsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+        
+        public static async ValueTask ToastrError(this IJSRuntimeExtension JsRuntime, string message)
+        {
+            await JsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+    }
+}
