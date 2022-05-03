81d5e666ba0c4dc7114a52fd9086c36b9005eace
KennedyMungai <kennedymungaifmab@gmail.com>
2022-05-03 09:58:06 +0300

    Data migrations failed because of some errors but isorait

--- Tangy_DataAccess/Category.cs
+++ Tangy_DataAccess/Category.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Tangy_DataAccess
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public DateTime CreatedDate { get; set; }
     }
 }
--- Tangy_DataAccess/Data/ApplicationDbContext.cs
+++ Tangy_DataAccess/Data/ApplicationDbContext.cs
@@ -7,10 +7,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Tangy_DataAccess.Data
 {
-    public DbSet<Category> Categories { get; set; }
-
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
